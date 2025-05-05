f7e69622debd991dcda255242ff8044c8ebd53d1
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-05-05 12:36:14 +0200

    monthly dependencies update

--- project.clj
+++ project.clj
@@ -4,13 +4,14 @@
                  [hiccup "2.0.0-RC3"]
                  [http-kit "2.8.0"]
                  [org.clojure/clojure "1.12.0"]
-                 [org.clojure/core.async "1.8.735"]
+                 [org.clojure/core.async "1.8.741"]
                  [org.clojure/core.match "1.1.0"]
                  [org.slf4j/slf4j-simple "2.0.17"]
                  [ring/ring-defaults "0.6.0"]
                  ;; pinned transitives
                  [org.clojure/tools.reader "1.5.2"]
-                 [ring/ring-core "1.13.0"]]
+                 [ring/ring-core "1.14.1"]
+                 [ring/ring-codec "1.3.0"]]
   :min-lein-version "2.0.0"
   :main ^:skip-aot t.core
   :target-path "target/%s"
@@ -24,6 +25,6 @@
                    :dependencies [[no.cjohansen/replicant "2025.03.27"]
                                   [com.bhauman/figwheel-main "0.2.20"]
                                   [com.nextjournal/beholder "1.0.2"]
-                                  [org.clojure/clojurescript "1.11.132"]
+                                  [org.clojure/clojurescript "1.12.38"]
                                   [org.clojure/tools.namespace "1.5.0"]]}}
   :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]})
